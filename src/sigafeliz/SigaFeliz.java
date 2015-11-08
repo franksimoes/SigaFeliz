fc774da787dd8fcc640a62abd9c6a6d849c0ba53
frank <frank@PC-WORK>
2015-11-08 11:23:27 -0200

    Corrigido o Menu de Entrada, crash quando o valor era alfanumérico: - tratamento todo refeito em String; - identifica somente as opções 1,2 e 3; - qualquer outro valor apresenta mensagem de erro para o usuário é volta ao Menu;

--- src/sigafeliz/SigaFeliz.java
+++ src/sigafeliz/SigaFeliz.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,6 +14,7 @@ package sigafeliz;
 /* Revisão e correção para atender as correções depois dos testes da Revisão 1
    revisado por Francisco Simões Filho
 */
+//import java.util.InputMismatchException; 
 import java.util.Scanner;
 
 public class SigaFeliz {
@@ -20,26 +22,29 @@ public class SigaFeliz {
     public static void main(String[] args) {
         //Declaracao de variaveis, vetores e scanner
         Scanner entradaopc = new Scanner(System.in);
-        int contador, opcao, assento, i;
+        int contador, assento, i;
+        String opcao;
         char tipoassento;
         int janela[] = new int[24];
         int corredor[] = new int[24];
         //Inicialização das variaveis opcao e assento 
-        opcao = 0;
+        opcao = "0";
         assento = 0;
       
         //O programa vai rodar até o usuário escolher a OPÇÃO 3 do menu inicial
-        while (opcao != 3) {
-
+        while ( !opcao.equals("3")) {
+            
             //Exibição do menu inicial
+            System.out.println("----------------------------");
             System.out.println("MENU - VENDA DE PASSAGENS \n1. Vender passagem \n2. Mapa de ocupacao \n3. Encerrar ");
-            opcao = entradaopc.nextInt();
-
+            System.out.println("----------------------------");
+            opcao = entradaopc.next();
             //OPÇÃO 1 - VENDA DE PASSAGENS
-            if (opcao == 1) {
+            if (opcao.equals("1")) {
                 System.out.println("Por favor digite o número da poltrona desejada");
                 //A variável assento foi iniciada como 0. Enquanto ela estiver nesse valor ou o usuário digitar uma opção de poltrona fora do range,
                 //o bloco continuará a ser executado. 
+              //if (entradaopc.hasNextInt()) {    
                 while (assento < 1 || assento > 24) {
                     assento = entradaopc.nextInt();
                     //Valida a entrada do usuário. Se estiver dentro do range 1~24, continua o programa, pedindo o tipo do assento
@@ -80,7 +85,7 @@ public class SigaFeliz {
                 // comprar outra passagem, o programa não preencha os vetores com valores anteriores de assentos.
                 assento = 0;
             //OPÇÃO 2 - MAPA DE ASSENTOS    
-            } else if (opcao == 2) {
+            } else if (opcao.equals("2")) {
                 System.out.println("JANELA              CORREDOR");
                 for (i = 0; i < 24; i++) {
                     //Variável de escopo local somente para output dos dados na mesma linha. As linhas comentadas funcionam, mas exibiriam
@@ -103,13 +108,17 @@ public class SigaFeliz {
                     System.out.println((i+1)+ " " + posicao1 + "              " + (i+1) + " " + posicao2);
                 }
             //OPÇÃO 3 - FIM DO PROGRAMA
-            } else if (opcao == 3) {
+            } else if (opcao.equals("3")) {
                 System.exit(0);
             } else {
+                System.out.println("\n\n\n");
+                System.out.println("--------------------------------------------");                
                 System.out.println("OPÇÃO INCORRETA, POR FAVOR ESCOLHA NOVAMENTE");
+                System.out.println("--------------------------------------------\n\n\n");
             }
         }
     }
 
     
 }
+
